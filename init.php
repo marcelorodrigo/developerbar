7cba6b3c96c0e0e6d42fc356b1f9d66720dfc56c
Marcelo Rodrigo <mrodrigow@gmail.com>
2011-05-31 22:05:20 -0300

    Module only loaded when outside of a PRODUCTION environment

--- init.php
+++ init.php
@@ -1,11 +1,13 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+if(Kohana::$environment !== Kohana::PRODUCTION)
+{	
+	// Static file serving (CSS, JS, images)
+	Route::set('developerbar/media', 'developerbar/media(/<file>)', array('file' => '.+'))
+		->defaults(array(
+			'controller' => 'developerbar',
+			'file'       => NULL,
+		));
 
-// Static file serving (CSS, JS, images)
-Route::set('developerbar/media', 'developerbar/media(/<file>)', array('file' => '.+'))
-	->defaults(array(
-		'controller' => 'developerbar',
-		'file'       => NULL,
-	));
-
-// Load Developerbar :)
-register_shutdown_function('developerbar::render');
+	// Load Developerbar :)
+	register_shutdown_function('developerbar::render');
+}
